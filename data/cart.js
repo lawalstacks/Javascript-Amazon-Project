9e872b04f00feff0b9a4fd4aa6bba9b265e958fe
lawalstacks <quazeemastic@gmail.com>
2024-04-17 11:19:37 +0100

    Added Dayjs library for deliveryOptions

--- data/cart.js
+++ data/cart.js
@@ -7,10 +7,12 @@ if(!cart){
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 0,
+      deliveryOptionId: '1'
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity: 0,
+      deliveryOptionId: '2'
     }
     ];
   
@@ -45,11 +47,11 @@ export function addToCart(productId){
        }
   else{
    cart.push({
-     productId,
-     quantity : Number(document.querySelector(`.js-quantity-selector-${productId}`).value)
+     productId: productId,
+     quantity : Number(document.querySelector(`.js-quantity-selector-${productId}`).value),
+     deliveryOptionId: '1'
    })
    }
-   saveToStorage();
 }
 
 export function removeFromCart(productId){
