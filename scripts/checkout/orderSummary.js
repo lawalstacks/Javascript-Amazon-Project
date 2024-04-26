14c6c28e6774f141882c3858599e0a0546bc7ccc
Kazeem Lawal <quazeemastic@gmail.com>
2024-04-26 01:10:33 +0100

    Make paymentSummary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,6 +4,7 @@ import {products,getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js'
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 calculateCartQuantity();
@@ -112,6 +113,7 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
         calculateCartQuantity();
     document.querySelector(`.js-cart-container-remove-${productId}`).remove();
         calculateCartQuantity();
+        renderPaymentSummary();
   });
 });
 
@@ -120,6 +122,7 @@ document.querySelectorAll('.js-delivery-option').forEach((element) => {
     const {productId, deliveryOptionId} = element.dataset;
     updateDeliveryOption(productId,deliveryOptionId);
     renderOrderSummary();
+    renderPaymentSummary();
   })
 });
 
